4174ba7acec8bf60fea780a189b1529dcab14a69
watpekin <henrique.watpequim@gmail.com>
2020-11-09 14:28:09 -0300

    Regra checkmatch

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -15,7 +15,10 @@ public abstract class ChessPiece extends Piece{
 	public Color getColor() {
 		return color;
 	}
-	
+	public ChessPosition getChessPosition() {
+		// pegar a posição
+		return ChessPosition.fromPosition(position);
+	}
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
